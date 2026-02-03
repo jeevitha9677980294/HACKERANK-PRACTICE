d8b2eae0e57b8478064d5867726e35e3c921e4b4
Balagv1114 <balaji_23csa16@kgkite.ac.in>
2026-02-03 09:50:29 +0530

    Day X: HackerRank practice

---
+++ Java Strings Introduction.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        
+        String a=sc.next();
+        String b=sc.next();
+        
+        System.out.println(a.length()+b.length());
+        if(a.compareTo(b)>0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        } 
+    
+String capa=a.substring(0,1).toUpperCase()+a.substring(1);
+String capb=b.substring(0,1).toUpperCase()+b.substring(1);
+
+System.out.println(capa +" "+capb);
+    }
+    }
---
+++ java substring.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        System.out.println(a.substring(start,end));
+        
+        
+        
+        
+    }
+}
