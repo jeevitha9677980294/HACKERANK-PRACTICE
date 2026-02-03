af64f1c60e4a1828aa0ac49ee0a8d2b32e944bf6
Balagv1114 <balaji_23csa16@kgkite.ac.in>
2026-02-03 09:07:13 +0530

    Day 6: Solved HackerRank array problems

---
+++ Java Datatypes.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String []argh) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();   
+
+        for(int i = 0; i < t; i++) {
+            try {
+                long n = sc.nextLong(); 
+
+                System.out.println(n + " can be fitted in:");
+
+                if(n >= -128 && n <= 127)
+                    System.out.println("* byte");
+
+                if(n >= -32768 && n <= 32767)
+                    System.out.println("* short");
+
+                if(n >= -2147483648 && n <= 2147483647)
+                    System.out.println("* int");
+
+                
+                System.out.println("* long");
+            }
+            catch(Exception e) {
+               
+                String bigNumber = sc.next(); 
+                System.out.println(bigNumber + " can't be fitted anywhere.");
+            }
+        }
+    }
+}
